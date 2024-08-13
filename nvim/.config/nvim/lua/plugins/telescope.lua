538a208b841a9ac28920bb983443dea9598d563b
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-08-13 23:11:29 +0530

    nvim: show hidden file from telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,13 +8,15 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         defaults = {
           file_ignore_patterns = {
-            "incude/",
-            "pyvenv%.cfg",
-            "claude_chats/",
-            "%.claudeignore",
-          }
+            ".git",
+          },
         },
         extensions = {
           ["ui-select"] = {
@@ -23,11 +25,11 @@ return {
         },
       })
 
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
       require("telescope").load_extension("ui-select")
     end,
