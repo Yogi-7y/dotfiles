461915f63e4359be6470c4cf37a2c0fd9efe9575
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-08-14 20:49:38 +0530

    wezterm: update font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,12 +3,13 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config = {
-  automatically_reload_config = true,
-  enable_tab_bar = false,
-  window_decorations = "RESIZE",
-  window_close_confirmation = "NeverPrompt",
-  font_size = 16,
-  color_scheme = "Catppuccin Mocha",
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_decorations = "RESIZE",
+	window_close_confirmation = "NeverPrompt",
+	font_size = 16,
+	color_scheme = "Catppuccin Mocha",
+	font = wezterm.font("JetBrains Mono", { weight = "Medium" }),
 }
 
 return config
