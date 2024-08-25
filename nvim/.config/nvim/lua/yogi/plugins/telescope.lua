a91fb722bf5751547f3a5daa897b1174efedccd6
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-08-25 10:59:46 +0530

    nvim: add absolute path in telescope fuzzy find temporary

--- nvim/.config/nvim/lua/yogi/plugins/telescope.lua
+++ nvim/.config/nvim/lua/yogi/plugins/telescope.lua
@@ -7,16 +7,29 @@ return {
 		tag = "0.1.5",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			require("telescope").setup({
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
+
+			telescope.setup({
 				pickers = {
 					find_files = {
-						hidden = true,
+						find_command = {
+							"fd",
+							"--type",
+							"f",
+							"--hidden",
+							"--follow",
+							"--exclude",
+							".git",
+							"--search-path",
+							".",
+							"--search-path",
+							"/Users/yogi/Development/personal_projects/notion_db_sdk",
+						},
 					},
 				},
 				defaults = {
-					file_ignore_patterns = {
-						".git",
-					},
+					path_display = { "smart" },
 				},
 				extensions = {
 					["ui-select"] = {
@@ -25,7 +38,6 @@ return {
 				},
 			})
 
-			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
