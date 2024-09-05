6618716f259c44ffe922f31475906b61fdc20ae3
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-09-05 19:43:21 +0530

    nvim: update catppuccin theme

--- nvim/.config/nvim/lua/yogi/plugins/catppucin.lua
+++ nvim/.config/nvim/lua/yogi/plugins/catppucin.lua
@@ -1,12 +1,33 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.o.termguicolors = true
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.o.termguicolors = true
+			vim.cmd.colorscheme("catppuccin-mocha")
+
+			local catppuccin = require("catppuccin")
+
+			catppuccin.setup({
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					treesitter = true,
+					harpoon = true,
+					mason = true,
+					noice = true,
+					dap = true,
+					dap_ui = true,
+					lsp_trouble = true,
+					which_key = true,
+					neotree = true,
+					telescope = {
+						enabled = true,
+					},
+				},
+			})
+		end,
+	},
 }
