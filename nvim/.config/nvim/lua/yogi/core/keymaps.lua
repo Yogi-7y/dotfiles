a865ac12ee2ec7c07c3f2230fe49a4f46368b4ff
Yogesh Parwani <yogesh.parwani@coindcx.com>
2024-09-18 08:59:04 +0530

    nvim: additional keymaps for windows

--- nvim/.config/nvim/lua/yogi/core/keymaps.lua
+++ nvim/.config/nvim/lua/yogi/core/keymaps.lua
@@ -1,6 +1,22 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Resize windows
+keymap.set("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "Increase window height" })
+keymap.set("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Decrease window height" })
+keymap.set("n", "<C-Right>", "<cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
+keymap.set("n", "<C-Left>", "<cmd>vertical resize +2<CR>", { desc = "Increase window width" })
+
+-- Split windows
+keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+
+-- Focus in and out of windows
+keymap.set("n", "<leader>wo", "<C-w>o", { desc = "Focus only on current window (close others)" })
+keymap.set("n", "<leader>ww", "<C-w>w", { desc = "Cycle through windows" })
+keymap.set("n", "<leader>wr", "<C-w>r", { desc = "Rotate windows" })
 
 -- Navigate vim panes
 keymap.set("n", "<c-k>", ":wincmd k<CR>", { desc = "Move to the pane above" })
