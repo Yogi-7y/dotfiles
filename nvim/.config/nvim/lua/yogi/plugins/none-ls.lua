8976214fb84816ba2fc1cb0f8ecb75b490175be9
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-09-22 08:44:32 +0530

    nvim: enforce formatting by none-ls and ignore lsp formatters

--- nvim/.config/nvim/lua/yogi/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/yogi/plugins/none-ls.lua
@@ -1,39 +1,48 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    local on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({
-              bufnr = bufnr,
-            })
-          end,
-        })
-      end
-    end
+		local format = function(bufnr)
+			vim.lsp.buf.format({
+				filter = function(client)
+					return client.name == "null-ls"
+				end,
+				bufnr = bufnr,
+			})
+		end
 
-    null_ls.setup({
-      on_attach = on_attach,
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.dart_format.with({
-          extra_args = function()
-            local user = vim.fn.expand("$USER")
-            local line_length = user:find("yogi") and 100 or 80
-            return { "--line-length", tostring(line_length) }
-          end,
-        }),
-      },
-    })
+		local on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						format(bufnr)
+					end,
+				})
+			end
+		end
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format file" })
-  end,
+		null_ls.setup({
+			on_attach = on_attach,
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.dart_format.with({
+					extra_args = function()
+						local user = vim.fn.expand("$USER")
+						local line_length = user:find("yogi") and 100 or 80
+						return { "--line-length", tostring(line_length) }
+					end,
+				}),
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", function()
+			format(0)
+		end, { desc = "Format file" })
+	end,
 }
