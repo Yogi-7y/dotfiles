5dd2e3c8531a0e444e8b2b9ca6841033f000465d
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-11-04 21:28:44 +0530

    chore: disable flutter-tools

--- nvim/.config/nvim/lua/yogi/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/yogi/plugins/lsp_config.lua
@@ -1,51 +1,51 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
-        on_attach = function(client, bufnr)
-          local filepath = vim.api.nvim_buf_get_name(bufnr)
-          if filepath:match("%.nvim%.lua$") then
-            vim.schedule(function()
-              vim.lsp.buf_detach_client(bufnr, client.id)
-              vim.notify("Lua LSP disabled for .nvim.lua file", vim.log.levels.INFO)
-            end)
-          else
-          end
-        end,
-        root_dir = function(fname)
-          if fname:match("%.nvim%.lua$") then
-            return nil
-          end
-          return lspconfig.util.find_git_ancestor(fname) or vim.loop.os_homedir()
-        end,
-      })
+			lspconfig.lua_ls.setup({
+				on_attach = function(client, bufnr)
+					local filepath = vim.api.nvim_buf_get_name(bufnr)
+					if filepath:match("%.nvim%.lua$") then
+						vim.schedule(function()
+							vim.lsp.buf_detach_client(bufnr, client.id)
+							vim.notify("Lua LSP disabled for .nvim.lua file", vim.log.levels.INFO)
+						end)
+					else
+					end
+				end,
+				root_dir = function(fname)
+					if fname:match("%.nvim%.lua$") then
+						return nil
+					end
+					return lspconfig.util.find_git_ancestor(fname) or vim.loop.os_homedir()
+				end,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover information" })
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-      vim.keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
-      vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Find references" })
-      vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-      vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-      vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover information" })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+			vim.keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Find references" })
+			vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+			vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+		end,
+	},
 }
