d3e36571f968e29081118727ded29c74190c1ffb
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-11-04 21:28:59 +0530

    chore: conditional check for neovim init

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,8 @@
-require("yogi.core.init")
-require("yogi.lazy")
-vim.o.exrc = true
-vim.o.secure = true
+local in_vscode = vim.g.vscode ~= nil
+
+if not in_vscode then
+	require("yogi.core.init")
+	require("yogi.lazy")
+	vim.o.exrc = true
+	vim.o.secure = true
+end
