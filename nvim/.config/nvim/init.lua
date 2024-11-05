1c0fe95ae78bdd0fa66aa15cdd3cc1bb59e3d472
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-11-05 11:21:05 +0530

    nvim: remove vscode check

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,4 @@
-local in_vscode = vim.g.vscode ~= nil
-
-if not in_vscode then
-	require("yogi.core.init")
-	require("yogi.lazy")
-	vim.o.exrc = true
-	vim.o.secure = true
-end
+require("yogi.core.init")
+require("yogi.lazy")
+vim.o.exrc = true
+vim.o.secure = true
