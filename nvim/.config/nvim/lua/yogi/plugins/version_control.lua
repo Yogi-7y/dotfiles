22be458edbbde9b0e45151ebc27dca09655ea1b8
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-11-07 16:06:04 +0530

    nvim: add git blame

--- nvim/.config/nvim/lua/yogi/plugins/version_control.lua
+++ nvim/.config/nvim/lua/yogi/plugins/version_control.lua
@@ -3,7 +3,18 @@ return {
 	config = function()
 		local gitsigns = require("gitsigns")
 
-		gitsigns.setup({})
+		gitsigns.setup({
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 1000,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
+			current_line_blame_formatter = "       <author>, <author_time:%R> • <summary>",
+		})
 
 		vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview git hunk" })
 		vim.keymap.set("n", "<leader>gt", gitsigns.toggle_current_line_blame, { desc = "Toggle git blame" })
