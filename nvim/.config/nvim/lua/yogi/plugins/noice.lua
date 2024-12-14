a8dbef1c64a4ffac74f19fb1b20dbd4e5e401d07
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-12-14 23:06:15 +0530

    nvim: comment out noice

--- nvim/.config/nvim/lua/yogi/plugins/noice.lua
+++ nvim/.config/nvim/lua/yogi/plugins/noice.lua
@@ -1,31 +1,32 @@
-return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-  },
-  config = function()
-    local noice = require("noice")
-
-    noice.setup({
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      presets = {
-        bottom_search = true,     -- use a classic bottom cmdline for search
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,       -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,   -- add a border to hover docs and signature help
-      },
-    })
-
-    vim.keymap.set("n", "<leader>nd", function()
-      noice.cmd("dismiss")
-    end, { desc = "Dismiss all Noice messages" })
-  end,
-}
+-- return {
+--   "folke/noice.nvim",
+--   event = "VeryLazy",
+--   dependencies = {
+--     "MunifTanjim/nui.nvim",
+--     "rcarriga/nvim-notify",
+--   },
+--   config = function()
+--     local noice = require("noice")
+--
+--     noice.setup({
+--       lsp = {
+--         override = {
+--           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--           ["vim.lsp.util.stylize_markdown"] = true,
+--           ["cmp.entry.get_documentation"] = true,
+--         },
+--       },
+--       presets = {
+--         bottom_search = true,     -- use a classic bottom cmdline for search
+--         long_message_to_split = true, -- long messages will be sent to a split
+--         inc_rename = false,       -- enables an input dialog for inc-rename.nvim
+--         lsp_doc_border = false,   -- add a border to hover docs and signature help
+--       },
+--     })
+--
+--     vim.keymap.set("n", "<leader>nd", function()
+--       noice.cmd("dismiss")
+--     end, { desc = "Dismiss all Noice messages" })
+--   end,
+-- }
+return {}
